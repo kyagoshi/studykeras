209e9eea97513153440af77d09fe27a10d4a0f4e
keisukemiyazaki <zakimiya.k0425@gmail.com>
2019-02-13 20:58:30 +0900

    3章終了

---
+++ .history/3-32_20190213202511.py
@@ -0,0 +1,62 @@
+from keras.datasets import boston_housing
+import numpy as np
+from keras import models
+from keras import layers
+import matplotlib.pyplot as plt
+from keras import backend as K
+
+(train_data, train_targets), (test_data, test_targets) = boston_housing.load_data()
+
+mean = train_data.mean(axis=0)
+train_data -= mean
+std = train_data.std(axis=0)
+train_data /= std
+test_data -= mean
+test_data /= std
+
+def bulid_model():
+  model = models.Sequential()
+  model.add(layers.Dense(64, activation='relu', input_shape=(train_data.shape[1],)))
+  model.add(layers.Dense(64, activation='relu'))
+  model.add(layers.Dense(1))
+
+  model.compile(optimizer='rmsprop', loss='mse', metrics=['mae'])
+  return model
+
+k = 4
+num_val_samples = len(train_data) // k
+num_epochs = 500
+all_mae_histories = []
+for i in range(k):
+  print('processing fold #', i)
+
+  val_data = train_data[i * num_val_samples: (i + 1) * num_val_samples]
+  val_targets = train_targets[i * num_val_samples: (i + 1) * num_val_samples]
+
+  partinal_train_data = np.concatenate(
+    [train_data[:i * num_val_samples],
+    train_data[(i + 1) * num_val_samples:]],
+    axis=0)
+
+  partinal_train_targets = np.concatenate(
+    [train_targets[:i * num_val_samples],
+    train_targets[(i + 1) * num_val_samples:]],
+    axis=0)
+
+  model = bulid_model()
+
+  history = model.fit(partinal_train_data, partinal_train_targets, validation_data=(val_data, val_targets), epochs=num_epochs, batch_size=1, verbose=0)
+  mae_history = history.history['val_mean_absolute_error']
+  all_mae_histories.append(mae_history)
+
+average_mae_history = [np.mean([x[i] for x in all_mae_histories]) for i in range(num_epochs)]
+
+model = bulid_model()
+
+model.fit(train_data, test_targets, epochs=80, batch_size=16, verbose=0)
+
+test_mse_score, test_mae_score = model.evaluate(test_data, test_targets)
+
+print(test_mae_score)
+
+K.clear_session()
